05913d27bd1577b008aeb0a1b7b786cbc2932384
Dreamonix <fabian.blank01@outlook.com>
2025-04-18 13:53:06 +0200

    feat(backend): add spring backend

---
+++ backend/src/test/java/com/whs/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whs.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
